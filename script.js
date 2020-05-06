415d0f7fbe2dcc9e82ba4ab15660898aa6bf094c
GerardD1 <61371136+GerardD1@users.noreply.github.com>
2020-05-06 17:55:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const actualTemp = 50;
-const desiredTemp = 70;
-
-if (actualTemp < desiredTemp) {
-    console.log("Run heat");
-} else if (actualTemp > desiredTemp) {
-    console.log("Run A/C");
-} else {
-    console.log("Standby");
+for (let i = 1; i < 11; i = i + 1) {
+    console.log(i);
 }
 
-const tempC = parseInt(prompt("Enter Celcius: "));
-const targetUnit = prompt("Enter in C, K, or F");
-let finalTemp;
+for (let i = 10; i > 0; i = i - 1) {
+    console.log(i);
+}
 
-switch (targetUnit) {
-    case "C":
-        finalTemp = tempC;
-        console.log(finalTemp);
-        break;
-    case "K":
-        finalTemp = tempC + 273.15;
-        console.log(finalTemp);
-        break;
-    case "F":
-        finalTemp = (tempC * (9 / 5)) + 32;
-        console.log(finalTemp);
-        break;
-    default:
-        console.log("You didn't type in a C, K, or F");
-} 
+let i = 1;
+
+while (i < 11) {
+    console.log(i++);
+}
+
+i = 10;
+
+while (i > 0) {
+    console.log(i--);
+}
+
+i = 1;
+
+do {
+    console.log(i++);
+}
+while (i < 11);
+
+i = 10;
+
+do {
+    console.log(i--);
+}
+while (i >0);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let number of numbers) {
+    console.log(number);
+}
